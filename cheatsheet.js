265324813c0433432d26b2a664a57f8706bc6e24
Michael <MFMaloney@gmail.com>
2016-04-28 18:11:58 -0400

    added cheat sheet and homework html

---
+++ cheatsheet.js
@@ -0,0 +1,68 @@
+/* Variable - A variable is a container that stands in for another value. 
+It is written as var when coding.*/
+
+var stuff = "things"
+
+var num = 23
+
+/* When debuggina a site adding comments, alerts and logging information to 
+the console can be extremely useful */
+
+// For single line comments put // at the beginning of the line
+// For multiline comments wrap the text in /*...*/
+
+//to log to a console, console.log() is used:
+
+console.log(This will show up in the console)
+
+//to have an alert pop up, use alert()
+
+alert(this is an alert)
+
+// There are 4 basic data types: String, Number, Boolean, Undefined
+
+//A string is information contained in quotes
+
+"This is a string"
+
+// Numbers are simply just that. All numbers in javascript are floats
+
+5, 88.9, etc...
+
+// Boolean is just a representation of weather a statement is true or false.
+
+// Undefined is when there is no value present
+
+// Arrays - Arrays are essential just a collection of data.
+// They can be all one data type or a combination.
+// Arrays are contained inside square brackets [...] and seperated by commas
+
+["string1", "string2", 4, 5]
+// Important to note when counting in an array, the first object is 0
+// In our example string1 would be 0 and string2 would be 1
+
+// Testing -  Testing is used to see if a statement is true or false
+// When running a test use === (thats 3 equal signs)
+
+5 === 7
+//this would obviously come back false
+
+"monkey" === "monkey"
+// this would come back true
+
+
+// Logic - logic is what determines the flow of your website.
+// It essentially has the computer do the thinking for you
+
+// Functions - A function is a piece of code that contains a number of 
+// arguments and returns a result that can be used later on or displayed
+// immediately.
+
+function stuff("things")
+	return alert(things)
+
+// to invoke the function use its name
+stuff("table")
+// this will create and alert that says table
+
+
